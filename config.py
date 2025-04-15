99d3f6c3918516cfcb26170efdf236444e8b57ce
Fiona Tan <107552356+ftjy@users.noreply.github.com>
2025-04-15 12:26:14 +0800

    added config file

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/portfoliowebapp'
