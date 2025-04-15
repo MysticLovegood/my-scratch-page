6de7487198ac3e1e2176977558fcf9ba139837b7
MysticLovegood <107552356+MysticLovegood@users.noreply.github.com>
2025-04-15 11:55:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
 class Config:
-    SECRET_KEY=os.environ.get('SECRET_KEY') or 'supersecretkey'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<username>:<password>@localhost:3306/portfoliowebapp'
